f862edb4d977ae7aa28ff16f43dfd938d4bc5126
Kiranvvl <86787729+Kiranvvl@users.noreply.github.com>
2025-07-02 10:50:30 +0530

    Update vite.config.js
    vite.config.js file modified

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // headers: {
-    //   'Cross-Origin-Opener-Policy': 'unsafe-none',
-    // },
-  },
+ 
 });
